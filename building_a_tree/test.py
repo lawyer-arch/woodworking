51ba6d9989204196c37c6ff1940c8e19db0fb624
Pavel Anikin <PavelDidko@gmail.com>
2025-03-19 17:57:43 +0300

    init and add package hexlet

--- building_a_tree/test.py
+++ building_a_tree/test.py
@@ -2,3 +2,12 @@ from hexlet import fs
 tree = fs.mkdir('/', [fs.mkfile('hexlet.log')], {'hidden': True})
 print(fs.get_name(tree))
 print(fs.get_meta(tree).get('hidden'))
+
+[file] = fs.get_children(tree)
+print(fs.get_name(file))
+# 'hexlet.log'
+fs.get_meta(file).get('unknown')
+
+# А вот так делать не надо
+# У файлов нет потомков
+fs.get_children(file)
